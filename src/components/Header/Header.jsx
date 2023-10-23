9e439f16dca96c0f083f6689c78c09abc55a7925
Vitaliy Korostylov <vitaliy.koros@gmail.com>
2023-10-23 17:19:10 +0300

    adds current user fetch check on header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,13 +3,18 @@ import { BtnTitle, BtnWrapper, LogOutBtn } from "../Buttons/Buttons.styled";
 import Logo from "../Logo/Logo";
 import { HeaderContainer, UserName, WelcomeText } from "./Header.styled";
 import { useDispatch, useSelector } from "react-redux";
-import { selectIsAuth, selectUser } from "src/redux/selectors";
+import {
+  selectIsAuth,
+  selectIsFetchingCurrentUser,
+  selectUser,
+} from "src/redux/selectors";
 import { FiLogOut } from "react-icons/fi";
 import { logOutThunk } from "src/redux/auth/authThunk";
 
 const Header = () => {
   const isAuth = useSelector(selectIsAuth);
   const username = useSelector(selectUser);
+  const isFetchingCurrentUser = useSelector(selectIsFetchingCurrentUser);
   const dispatch = useDispatch();
 
   const onLogOut = () => {
@@ -17,29 +22,33 @@ const Header = () => {
   };
 
   return (
-    <HeaderContainer>
-      <Logo />
+    <>
+      {!isFetchingCurrentUser && (
+        <HeaderContainer>
+          <Logo />
 
-      {isAuth && username ? (
-        <BtnWrapper>
-          <UserName>
-            <WelcomeText>Welcome,</WelcomeText> {username}
-          </UserName>
-          <LogOutBtn type="button" onClick={onLogOut}>
-            <FiLogOut color="rgb(178, 159, 126)" size={24} />
-          </LogOutBtn>
-        </BtnWrapper>
-      ) : (
-        <BtnWrapper>
-          <HeaderBtn to="/login">
-            <BtnTitle>Log In</BtnTitle>
-          </HeaderBtn>
-          <HeaderBtn to="/signup" filled>
-            <BtnTitle>Sign Up</BtnTitle>
-          </HeaderBtn>
-        </BtnWrapper>
+          {isAuth && username ? (
+            <BtnWrapper>
+              <UserName>
+                <WelcomeText>Welcome,</WelcomeText> {username}
+              </UserName>
+              <LogOutBtn type="button" onClick={onLogOut}>
+                <FiLogOut color="rgb(178, 159, 126)" size={24} />
+              </LogOutBtn>
+            </BtnWrapper>
+          ) : (
+            <BtnWrapper>
+              <HeaderBtn to="/login">
+                <BtnTitle>Log In</BtnTitle>
+              </HeaderBtn>
+              <HeaderBtn to="/signup" filled>
+                <BtnTitle>Sign Up</BtnTitle>
+              </HeaderBtn>
+            </BtnWrapper>
+          )}
+        </HeaderContainer>
       )}
-    </HeaderContainer>
+    </>
   );
 };
 
--- src/redux/selectors.js
+++ src/redux/selectors.js
@@ -4,3 +4,5 @@ export const selectDealsError = (state) => state.deals.error;
 export const selectIsAuth = (state) => state.auth.isAuth;
 export const selectUser = (state) => state.auth.username;
 export const selectAccessToken = (state) => state.auth.token;
+export const selectIsFetchingCurrentUser = (state) =>
+  state.auth.selectIsFetchingCurrentUser;
